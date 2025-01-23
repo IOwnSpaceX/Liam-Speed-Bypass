914a6299af276706dda8d1d981252e5d8a6e3d88
Liam <108475958+IOwnSpaceX@users.noreply.github.com>
2025-01-22 21:17:12 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Liam'
+description 'Speed Bypass'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
